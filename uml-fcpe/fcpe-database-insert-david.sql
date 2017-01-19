7b28a4d147aa073f55a68a6f123f610328be164f
david.cdi13 <david.cdi13@PC-NA-13.formation.imie>
2017-01-19 16:16:48 +0100

    dav : UseCase Diagram + WBS + insert db

---
+++ uml-fcpe/fcpe-database-insert-david.sql
@@ -0,0 +1,29 @@
+use fcpe;
+
+-- Proposition
+insert proposition (id,libelle) values (1,'Oui');
+insert proposition (id,libelle) values (2,'Non');
+insert proposition (id,libelle) values (3,'1');
+insert proposition (id,libelle) values (4,'2');
+insert proposition (id,libelle) values (5,'3');
+insert proposition (id,libelle) values (6,'4');
+insert proposition (id,libelle) values (7,'5');
+insert proposition (id,libelle) values (8,'Bon');
+insert proposition (id,libelle) values (9,'Acceptable');
+insert proposition (id,libelle) values (10,'Mauvais');
+insert proposition (id,libelle) values (11,'Bonne');
+insert proposition (id,libelle) values (12,'Excessive');
+insert proposition (id,libelle) values (13,'Insuffisante');
+
+-- QuestionProposition
+-- insert questionproposition (id,id_propo) values (,);
+
+-- Reponse
+-- insert reponse (reponse_principale,reponse_secondaire,choix_proposition,date,id_campagne,id_question,id_eleve)
+-- values ('',null,);
+
+-- ReponseProposition
+-- insert reponseproposition (id,id_reponse) value (1,1,1);
+
+
+
