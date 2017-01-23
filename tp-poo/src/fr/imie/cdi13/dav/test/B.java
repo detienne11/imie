056f2826b14910b66395b548a5e4a8859325c401
david.cdi13 <david.cdi13@PC-NA-13.formation.imie>
2017-01-23 17:10:47 +0100

    dav : B corrige

--- tp-poo/src/fr/imie/cdi13/dav/test/B.java
+++ tp-poo/src/fr/imie/cdi13/dav/test/B.java
@@ -6,10 +6,9 @@ public class B extends A {
 	
 	@Override
 	public AInterface function1(String value) {
-<<<<<<< HEAD
+
 		// new commentaire
-=======
->>>>>>> branch 'master' of https://github.com/detienne11/imie.git
+
 		return  new B();
 		
 	}
