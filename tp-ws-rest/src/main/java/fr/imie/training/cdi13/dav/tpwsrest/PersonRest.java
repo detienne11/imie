27a19866bcc494670a959758338506df08ddeebe
david.cdi13 <david.cdi13@PC-NA-13.formation.imie>
2017-03-16 15:53:05 +0100

    dav : tp ws rest

---
+++ tp-ws-rest/src/main/java/fr/imie/training/cdi13/dav/tpwsrest/PersonRest.java
@@ -0,0 +1,76 @@
+package fr.imie.training.cdi13.dav.tpwsrest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import fr.imie.training.cdi13.dav.tpjpa.api.Person;
+import fr.imie.training.cdi13.dav.tpjpa.api.PersonDAO;
+import fr.imie.training.cdi13.dav.tpjpa.impl.PersonDAOImpl;
+
+
+
+@Path("/person")
+@Produces(MediaType.APPLICATION_JSON)
+public class PersonRest {
+	
+	private PersonDAO personDAO = null;
+	
+	private PersonDAO getPersonDAO(){
+		if (this.personDAO == null){
+			this.personDAO = new PersonDAOImpl();
+		}
+		return this.personDAO;
+	}
+	
+	@GET
+	@Path("/ping")
+	public Response ping(){
+		return Response.ok("--PING--").build();
+	}
+	
+	@GET
+	public List<Person> findAll(){
+		List<Person> personList = this.getPersonDAO().find();
+//		List<Person> personList = new ArrayList<>();
+		return personList;
+	}
+	
+	@GET
+	@Path("{id}")
+	public Response findById(@PathParam("id") Integer id){
+		Person person = new Person();
+		person.setFirstName("Dark");
+		person.setLastName("Vador");
+		System.out.println(person);
+		return Response.ok(person).build();
+	}
+	
+	@POST
+	public Response create(Person person){		
+		System.out.println("--POST--" + person);		
+		return Response.ok("--POST--" + person).build();
+	}
+	
+	@PUT
+	public Response update(Person person){
+		System.out.println("--PUT--" + person);
+		return Response.ok("--PUT--" + person).build();
+	}
+	
+	@DELETE
+	@Path("{id}")
+	public Response delete(@PathParam("id") Integer id){
+		System.out.println("--DELETE--" + id);
+		return Response.ok("--DELETE--").build();
+	}
+}
---
+++ tp-ws-rest/src/main/java/fr/imie/training/cdi13/dav/tpwsrest/dto/Person.java
@@ -0,0 +1,40 @@
+package fr.imie.training.cdi13.dav.tpwsrest.dto;
+
+public class Person {
+
+	private Integer id;
+
+	private String firstName;
+
+	private String lastName;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
+}
